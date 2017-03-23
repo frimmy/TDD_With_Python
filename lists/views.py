94f4a5b48705fd2cc82e3665d4396bb143d77077
Adrian <adrian.frimpong@gmail.com>
2017-03-23 17:25:05 -0400

    adding files for a hard reset..

--- lists/views.py
+++ lists/views.py
@@ -19,22 +19,20 @@ def view_list(request, list_id):
             item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect('/lists/{}/'.format(list_.id))
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
-            return render(request, 'home.html', {"error": error})
-
-    return render(request, 'list.html', {'list': list_, 'error': error})
+    form = ItemForm()
+    return render(request, 'list.html', {
+        'list': list_, "form": form, "error": error
+        })
 
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['text'], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
